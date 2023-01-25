f172bd727a5ebfc34645e2c644542cd5467d6392
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-01-25 12:50:39 +0200

    generated app from scratch

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+const rootMain = require('./main.root');
+const { mergeConfig } = require('vite');
+const viteTsConfigPaths = require('vite-tsconfig-paths').default;
+
+module.exports = {
+  ...rootMain,
+  core: { ...rootMain.core, builder: '@storybook/builder-vite' },
+  stories: [
+    ...rootMain.stories,
+    '../src/app/**/*.stories.mdx',
+    '../src/app/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [...rootMain.addons],
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      plugins: [
+        viteTsConfigPaths({
+          root: './../',
+        }),
+      ],
+    });
+  },
+};
